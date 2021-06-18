713ac10996e5f5d33176efd615743ff4ee780953
Rishu Patel <rishupatel04@gmail.com>
2021-06-18 21:31:00 +0530

    add 404 view

---
+++ src/views/error/NotFound.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Result, Button } from 'antd';
+
+
+
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+    <Result
+      style={{color: "black"}}
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to="/">
+          <Button type="primary">Back Home</Button>
+        </Link>
+      }
+    />
+    </div>
+  )
+}
+
+export default NotFound

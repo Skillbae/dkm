f3137a279195f3783ff08776c42b78eba1f98d75
Rishu Patel <rishupatel04@gmail.com>
2021-06-18 21:38:36 +0530

    add views

---
+++ src/views/index.js
@@ -0,0 +1,36 @@
+export { default as Home } from './home/index';
+
+
+
+export {default as Shop} from './shop/index'
+export {default as Basket} from './basket/index'
+export { default as ViewProduct } from './view_product/index'
+
+
+export { default as Dashboard } from './admin/dashboard';
+export { default as AddProduct } from './admin/add_product';
+export { default as Products } from './admin/products/Products';
+export { default as Users } from './admin/users/Users';
+
+
+
+
+// export { default as EditAccount } from './account/edit_account';
+// export { default as UserAccount } from './account/user_account';
+// 
+
+// export { default as EditProduct } from './admin/edit_product';
+// export { default as ForgotPassword } from './auth/forgot_password';
+// export { default as SignIn } from './auth/signin';
+// export { default as SignUp } from './auth/signup';
+// export { default as CheckOutStep1 } from './checkout/step1';
+// export { default as CheckOutStep2 } from './checkout/step2';
+// export { default as CheckOutStep3 } from './checkout/step3';
+// export { default as PageNotFound } from './error/PageNotFound';
+// export { default as FeaturedProducts } from './featured';
+
+// export { default as RecommendedProducts } from './recommended';
+// export { default as Search } from './search';
+
+// ;
+

be4b86ecc798fd5f998536a0e20e646c00a9fb29
Rishu Patel <rishupatel04@gmail.com>
2021-06-18 18:56:14 +0530

    ➕ add firebase config

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7UyI6SCZV1M7nScWAKyvfSM1Jp0TlN0I",
+  authDomain: "react-ecommerce-114a8.firebaseapp.com",
+  projectId: "react-ecommerce-114a8",
+  storageBucket: "react-ecommerce-114a8.appspot.com",
+  messagingSenderId: "1891104351",
+  appId: "1:1891104351:web:2707abccc31471ae664dea",
+  measurementId: "G-RNM0VRKDF8"
+};
+
+export default firebaseConfig;
